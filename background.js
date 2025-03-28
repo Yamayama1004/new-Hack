b28bb5578e4d4356ca2aad84fd79c0bd7f53120e
Yama <c0a23162fe@edu.teu.ac.jp>
2025-03-28 12:09:47 +0900

    background.jsの修正

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onStartup.addListener(() => {
-  const youtubeUrl = "https://www.youtube.com/watch?v=IhOLjkOytss"; // ここに再生したいYouTube動画のIDを入力
+  const youtubeUrl = "https://www.youtube.com/watch?v=IhOLjkOytss";
 
   chrome.tabs.create({
     url: youtubeUrl,
