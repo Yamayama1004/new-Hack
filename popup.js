29f7d4b4cd7f7bfb541b3fd27eb74e6139802350
Yama <c0a23162fe@edu.teu.ac.jp>
2025-03-28 11:24:31 +0900

    popup.js,popup.htmlの作成

---
+++ popup.js
@@ -0,0 +1,7 @@
+/*
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("myButton").addEventListener("click", function () {
+      alert("ボタンがクリックされました！");
+    });
+  });
+*/
