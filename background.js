4aa32caf8a38e50628cdd07afcf3f8612bd55657
Yama <c0a23162fe@edu.teu.ac.jp>
2025-04-03 23:27:05 +0900

    エラー解消のための奮闘３
    大分大幅に変更を行った
    oauth2の導入

--- background.js
+++ background.js
@@ -2,33 +2,32 @@ const API_KEY = "AIzaSyBMxCRIyqgsKwRg6XUsQ_MmYBBLlQHs2rc";
 const searchTerm = "作業用BGM";
 let youtubeUrl = null;
 
-function initClient() {
-  gapi.client.init({
-    apiKey: API_KEY,
-    discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
-  }).then(() => {
-    searchVideos(searchTerm);
-  }).catch(error => {
-    console.error('APIクライアントの初期化エラー:', error);
-  });
+function authenticateAndSearch(){
+  chrome.identity.getAuthToken({interactive:true}, function(token){
+    if(chrome.runtime.lastError){
+      console.error('認証エラー:', chrome.runtime.lastError);
+      return;
+    }
+    searchVideos(token);
+  })
 }
 
-function searchVideos(query) {
-  gapi.client.youtube.search.list({
-    part: 'id',
-    q: query,
-    maxResults: 3,
-    type: 'video'
-  }).then(response => {
-    const videoIds = response.result.items.map(item => item.id.videoId).filter(id => id);
+function searchVideos(accessToken) {
+  const url = `https://www.googleapis.com/youtube/v3/search?part=id&q=<span class="math-inline">\{encodeURIComponent\(searchTerm\)\}&maxResults\=3&type\=video&key\=</span>{API_KEY}`;
 
-
-    if (videoIds.length > 0) {
+  fetch(url, {
+    headers: {
+      'Authorization': `Bearer ${accessToken}`
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    if(data.items && data.items.length > 0){
+      const videoIds = data.items.map(item => item.id.videoId).filter(id => id);
       const randomIndex = Math.floor(Math.random() * videoIds.length);
       const randomVideoId = videoIds[randomIndex];
-      console.log('ランダムに選択した動画ID:', randomVideoId);
       youtubeUrl = `https://www.youtube.com/watch?v=${randomVideoId}`;
-      createStartupTab();
+      chrome.tabs.create({url:youtubeUrl, active:false});
     } else {
       console.log('検索結果が見つかりませんでした。');
     }
@@ -37,15 +36,6 @@ function searchVideos(query) {
   });
 }
 
-function createStartupTab(){
-  if (youtubeUrl){
-    chrome.tabs.create({
-      url: youtubeUrl,
-      active: false
-  });
-  }
-}
-
 chrome.runtime.onStartup.addListener(() =>{
-  initClient();
+  authenticateAndSearch();
 });
