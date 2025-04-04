4438029db5363ccba79cbaf998cc09f9955b8093
Yama <c0a23162fe@edu.teu.ac.jp>
2025-04-05 03:06:38 +0900

    設定画面の追加

---
+++ background.js
@@ -0,0 +1,15 @@
+chrome.storage.sync.get(function_l, function (data) {
+    const isFunction1s = data.function1s !== undefined ? data.function1s : true;
+    const isFunction2s = data.function2s !== undefined ? data.function2s : true;
+  
+    if (isFunction1s){
+        //処理
+    }
+
+    if (isFunction2s){
+        //処理
+    }
+
+
+
+});
--- popup.js
+++ popup.js
@@ -1,6 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("myButton").addEventListener("click", function () {
-      alert("ボタンがクリックされました！");
-    });
+  const functionSettingForm = document.getElementById("function-setting");
+  const function1CB = document.getElementById("function1");
+  const function2CB = document.getElementById("function2");
+  const function_l = ["function1s", "function2s"]
+
+  chrome.storage.sync.get(function_l, function (data) {
+    function1CB.checked = data.function1s !== undefined ? data.function1s : true;
+    function2CB.checked = data.function2s !== undefined ? data.function2s : true;
   });
-  
+
+  function1CB.addEventListener('change', function () {
+    chrome.storage.sync.set({ "function1s": this.checked });
+  });
+
+  function2CB.addEventListener('change', function () {
+    chrome.storage.sync.set({ "function2s": this.checked });
+  });
+});
+
