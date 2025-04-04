dab35355e3da63457bfebf30d41cb4a5e69722af
Yama <c0a23162fe@edu.teu.ac.jp>
2025-04-04 16:20:50 +0900

    完成版（仮）
    ・APIの保護
    ・chrom立ち上げ時、及び拡張機能インストール時にyoutubeから「作業用BGM」の検索結果上位３つのうちランダムで一つ再生する

--- background.js
+++ background.js
@@ -1,18 +1,18 @@
 const searchTerm = "作業用BGM";
 let youtubeUrl = null;
 let apiKey = null;
+let startFlag = false;
 
 fetch('./config.json')
   .then(response => response.json())
   .then(config => {
     apiKey = config.youtubeApiKey;
-    authenticateAndSearch();
   });
 
 
-function authenticateAndSearch(){
-  chrome.identity.getAuthToken({interactive:true}, function(token){
-    if(chrome.runtime.lastError){
+function authenticateAndSearch() {
+  chrome.identity.getAuthToken({ interactive: true }, function (token) {
+    if (chrome.runtime.lastError) {
       console.error('認証エラー:', chrome.runtime.lastError);
       return;
     }
@@ -20,30 +20,49 @@ function authenticateAndSearch(){
   })
 }
 
+function changeTabs() {
+  chrome.tabs.query({ currentWindow: true }, function (tabs) {
+    if (tabs.length > 0) {
+      const firstTab = tabs[0];
+      setTimeout(function(){
+        chrome.tabs.update(firstTab.id, { active: true });
+      }, 1000);
+    } else {
+      console.log('開いているタブがありません。');
+    }
+  });
+}
+
 function searchVideos(accessToken, apiKey) {
-  const url = "https://www.googleapis.com/youtube/v3/search?part=id&q="+encodeURIComponent(searchTerm)+"&maxResults=3&type=video&key="+apiKey;
+  const url = "https://www.googleapis.com/youtube/v3/search?part=id&q=" + encodeURIComponent(searchTerm) + "&maxResults=3&type=video&key=" + apiKey;
 
   fetch(url, {
     headers: {
       'Authorization': `Bearer ${accessToken}`
     }
   })
-  .then(response => response.json())
-  .then(data => {
-    if(data.items && data.items.length > 0){
-      const videoIds = data.items.map(item => item.id.videoId).filter(id => id);
-      const randomIndex = Math.floor(Math.random() * videoIds.length);
-      const randomVideoId = videoIds[randomIndex];
-      youtubeUrl = "https://www.youtube.com/watch?v="+randomVideoId;
-      chrome.tabs.create({url:youtubeUrl, active:true});
-    } else {
-      console.log('検索結果が見つかりませんでした。');
-    }
-  }).catch(error => {
-    console.error('検索エラー:', error);
-  });
+    .then(response => response.json())
+    .then(data => {
+      if (data.items && data.items.length > 0) {
+        const videoIds = data.items.map(item => item.id.videoId).filter(id => id);
+        const randomIndex = Math.floor(Math.random() * videoIds.length);
+        const randomVideoId = videoIds[randomIndex];
+        youtubeUrl = "https://www.youtube.com/watch?v=" + randomVideoId;
+        chrome.tabs.create({ url: youtubeUrl, active: true });
+        changeTabs();
+      } else {
+        console.log('検索結果が見つかりませんでした。');
+      }
+    }).catch(error => {
+      console.error('検索エラー:', error);
+    });
 }
 
-chrome.runtime.onStartup.addListener(() =>{
+chrome.runtime.onInstalled.addListener(() => {
+  authenticateAndSearch();
+});
+
+
+chrome.runtime.onStartup.addListener(() => {
   authenticateAndSearch();
 });
