74c9b1b0d22caa1edd55b3ca9f0ce1038fd4f2da
Yama <c0a23162fe@edu.teu.ac.jp>
2025-04-06 01:58:28 +0900

    UIの作成

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 chrome.storage.sync.get(function_l, function (data) {
-    const isFunction1s = data.function1s !== undefined ? data.function1s : true;
-    const isFunction2s = data.function2s !== undefined ? data.function2s : true;
+    const isFunction1s = data.function1s !== undefined ? data.function1s : false;
+    const isFunction2s = data.function2s !== undefined ? data.function2s : false;
+    const isFunction3s = data.function3s !== undefined ? data.function3s : false;
   
     if (isFunction1s){
         //処理
@@ -8,6 +9,10 @@ chrome.storage.sync.get(function_l, function (data) {
 
     if (isFunction2s){
         //処理
+    }
+
+    if(isFunction3s){
+      //処理
     }
 
 
