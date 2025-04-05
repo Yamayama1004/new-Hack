bc3bbb2187d2a9caf42485af5a790400ce143e57
Yama <c0a23162fe@edu.teu.ac.jp>
2025-04-05 18:06:33 +0900

    途中２

--- popup.js
+++ popup.js
@@ -3,20 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
   const function1CB = document.getElementById("function1");
   const function2CB = document.getElementById("function2");
   const function3CB = document.getElementById("function3");
-  const function_l = ["function1s", "function2s"]
+  if (typeof chrome != "undefind" && chrome.storage) {
+    chrome.storage.sync.get(["function1s", "function2s", "function3s"], function (data) {
+      function1CB.checked = data.function1s !== undefined ? data.function1s : false;
+      function2CB.checked = data.function2s !== undefined ? data.function2s : false;
+      function3CB.checked = data.function3s !== undefined ? data.function3s : false;
+    });
 
-  chrome.storage.sync.get(function_l, function (data) {
-    function1CB.checked = data.function1s !== undefined ? data.function1s : false;
-    function2CB.checked = data.function2s !== undefined ? data.function2s : false;
-    function3CB.checked = data.function3s !== undefined ? data.function
-  });
+    function1CB.addEventListener('change', function () {
+      chrome.storage.sync.set({ "function1s": this.checked });
+    });
 
-  function1CB.addEventListener('change', function () {
-    chrome.storage.sync.set({ "function1s": this.checked });
-  });
+    function2CB.addEventListener('change', function () {
+      chrome.storage.sync.set({ "function2s": this.checked });
+    });
 
-  function2CB.addEventListener('change', function () {
-    chrome.storage.sync.set({ "function2s": this.checked });
-  });
+    function3CB.addEventListener('change', function () {
+      chrome.storage.sync.set({ "function3s": this.checked });
+    });
+  }
 });
 
